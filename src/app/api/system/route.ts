4ead1a3d22133f3b5c3fe3ac49e5f11e476ec401
markshawn2020 <shawninjuly@gmail.com>
2024-03-01 23:25:35 +0800

    avoid build update

--- src/app/api/system/route.ts
+++ src/app/api/system/route.ts
@@ -4,30 +4,36 @@ import { NextRequest, NextResponse } from "next/server"
 export async function GET(req: NextRequest) {
   const payload: string[] = []
 
-  await new Promise((resolve, reject) => {
-    const cmd = spawn("which yarn && yarn -v && yarn update", { shell: true })
+  /**
+   * 防止被编译的时候运行
+   */
+  if(new URL(req.url).searchParams.get("update")) {
+    await new Promise((resolve, reject) => {
+      const cmd = spawn("which yarn && yarn -v && yarn update", { shell: true })
 
-    cmd.stdout.on("data", (data: Buffer) => {
-      console.log(`[cmd] stdout: ${data.toString()}`)
-      // payload.stdOut.push(typeof data)
-      payload.push(data.toString())
-    })
+      cmd.stdout.on("data", (data: Buffer) => {
+        console.log(`[cmd] stdout: ${data.toString()}`)
+        // payload.stdOut.push(typeof data)
+        payload.push(data.toString())
+      })
 
-    cmd.stderr.on("data", (data: string) => {
-      console.log(`[cmd] stderr: ${data}`)
-      payload.push(data)
-    })
+      cmd.stderr.on("data", (data: string) => {
+        console.log(`[cmd] stderr: ${data}`)
+        payload.push(data)
+      })
 
-    cmd.on("error", (error) => {
-      console.log(`[cmd] error: ${error.message}`)
-      payload.push(error.message)
-    })
+      cmd.on("error", (error) => {
+        console.log(`[cmd] error: ${error.message}`)
+        payload.push(error.message)
+      })
 
-    cmd.on("close", (code) => {
-      console.log(`[cmd] close: child process exited with code ${code}`)
-      resolve(true)
+      cmd.on("close", (code) => {
+        console.log(`[cmd] close: child process exited with code ${code}`)
+        resolve(true)
+      })
     })
-  })
+  }
+
 
   return new Response(payload.join("\n"), { status: 200 })
 }
