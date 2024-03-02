e7d3ac89e59cb1e9631e8161e8db55741ff5c177
markshawn2020 <shawninjuly@gmail.com>
2024-03-02 13:15:33 +0800

    archive: function with this approach

--- src/store/conversation-v2.ts
+++ src/store/conversation-v2.ts
@@ -17,7 +17,8 @@ export interface IConversationStore {
   conversations: IConversationClient[]
   currentConversationId: string | null
 
-  readonly currentConversation: IConversationClient | null
+  currentConversation: () => IConversationClient | null
+  pApps: () => IPAppClient[]
 
   /**
    * == actions
@@ -74,87 +75,89 @@ export interface IConversationStore {
   ) => void
 }
 
-export class ConversationStore implements IConversationStore {
-  currentConversationId: string|null = null
-  conversations: IConversationClient[] = []
-  persistedPApps: IPAppClient[] = []
-  private get currentConversation() {
-    return this.conversations.find((c) => c.id === this.currentConversationId) ?? null
-  }
-  private get pApps() {
-    return this.currentConversation?.pApps ?? this.persistedPApps
-  }
+export const conversationStore = proxy<IConversationStore>({
+  persistedPApps: [],
+  conversations: [],
+  currentConversationId: null,
+  currentConversation() {
+    return (
+      this.conversations.find((c) => c.id === this.currentConversationId) ??
+      null
+    )
+  },
+  pApps() {
+    return this.currentConversation()?.pApps ?? this.persistedPApps
+  },
 
-  public addPAppInHomePage (pApp: IPAppClient)  {
+  addPAppInHomePage(pApp) {
     console.log("addPAppInHomePage: ", { pApp })
-if (this.persistedPApps.length >= 3) return toast.error("最多只支持3个")
-this.persistedPApps.push(pApp)
-}
+    if (this.persistedPApps.length >= 3) return toast.error("最多只支持3个")
+    this.persistedPApps.push(pApp)
+  },
 
-public useAddPAppInChatLayout () {
-      const addPApp = api.llm.addPApp.useMutation()
-      return async (pApp: IPAppClient) => {
-        if(!this.currentConversationId) return toast.error("会话尚未初始化")
-        if (this.pApps.length >= 3) return toast.error("最多只支持3个")
-        // optimistic update
-        this.pApps.push(pApp)
-        await addPApp.mutateAsync({
-          conversationId: this.currentConversationId,
-          id: pApp.id,
-          modelId: pApp.modelId,
-          title: pApp.title,
-        })
-      }
-}
-
-    public delPAppInHomePage (pAppId: string) {
-  if (this.pApps.length <= 1) return toast.error("至少需要一个")
-  remove(this.pApps, (i) => i.id === pAppId)
-}
-
-   public useDelPAppInChatLayout () {
-      const delPApp = api.llm.delPApp.useMutation()
-    return (pAppId: string) => {
-      if(!this.currentConversationId) return toast.error("会话尚未初始化")
-
-      if (this.pApps.length <= 1) return toast.error("至少需要一个")
+  useAddPAppInChatLayout: () => (pApp, conversation) => {
+    const addPApp = api.llm.addPApp.useMutation()
+    return async () => {
+      if (conversation.pApps.length >= 3) return toast.error("最多只支持3个")
+      const { id: conversationId } = conversation
+      console.log("addPAppInChatLayout: ", { pApp, conversationId })
       // optimistic update
-      remove(this.pApps, (i) => i.id === pAppId)
-      delPApp.mutate({ conversationId: this.currentConversationId, pAppId })
+      conversation.pApps.push(pApp)
+      await addPApp.mutateAsync({
+        conversationId,
+        id: pApp.id,
+        modelId: pApp.modelId,
+        title: pApp.title,
+      })
     }
-   }
+  },
 
-    public useAddConversation () {
-  const router = useRouter()
-  return () => {
-    const conversationId = nanoid()
+  delPAppInHomePage(pAppId) {
+    if (this.pApps().length <= 1) return toast.error("至少需要一个")
+    remove(this.pApps(), (i) => i.id === pAppId)
+  },
+
+  useDelPAppInChatLayout: () => (pAppId, conversation) => {
+    const { pApps, id: conversationId } = conversation
+    if (pApps.length <= 1) return toast.error("至少需要一个")
     // optimistic update
-    router.replace(`/tt/${conversationId}`)
-    this.conversations.splice(0, 0, {
-      id: conversationId,
-      pApps: this.pApps,
-      messages: [],
-    })
-    this.currentConversationId = conversationId
+    remove(pApps, (i) => i.id === pAppId)
+    const delPApp = api.llm.delPApp.useMutation()
+    delPApp.mutate({ conversationId, pAppId })
+  },
 
-    api.llm.createConversation
+  useAddConversation() {
+    const router = useRouter()
+    return () => {
+      const conversationId = nanoid()
+      // optimistic update
+      router.replace(`/tt/${conversationId}`)
+      const pApps = this.pApps()
+      this.conversations.splice(0, 0, {
+        id: conversationId,
+        pApps,
+        messages: [],
+      })
+      this.currentConversationId = conversationId
+
+      api.llm.createConversation
         .useMutation({
           onError: () => {
             // todo: more friendly alert dialog
             toast.error(
-                "不好意思，新建会话失败，请刷新后再试，该会话将被重置！",
+              "不好意思，新建会话失败，请刷新后再试，该会话将被重置！",
             )
           },
         })
         .mutate({
-          pApps: this.pApps,
+          pApps,
           type: "LLM",
         })
-    return conversationId
-  }
-}
+      return conversationId
+    }
+  },
 
-    public useDelConversation () {
+  useDelConversation() {
     return (conversationId: string) => {
       // optimistic update
       remove(this.conversations, (c) => c.id === conversationId)
@@ -162,57 +165,57 @@ public useAddPAppInChatLayout () {
         this.currentConversationId = null
 
       void api.llm.delConversation
-          .useMutation({})
-          .mutateAsync({ conversationId })
-          .catch((error) => {
-            console.error(error)
-            toast.error("删除失败！")
-          })
+        .useMutation({})
+        .mutateAsync({ conversationId })
+        .catch((error) => {
+          console.error(error)
+          toast.error("删除失败！")
+        })
     }
-  }
+  },
 
-   public useQueryInChatLayout () {
-    return (query: string) => {
-      if(!this.currentConversation) return toast.error("会话尚未初始化")
-      const conversationId = this.currentConversation.id
+  useQueryInChatLayout: () => (query, conversation) => {
+    const { id: conversationId, messages, pApps } = conversation
 
-      // optimistic update
-      this.currentConversation.messages.push({
-        id: nanoid(),
-        updatedAt: new Date(),
-        content: query,
-        role: "user",
-        conversationId,
-        pAppId: null,
-        parentId: null,
-      })
-
-      api.llm.queryConversation
-          .useMutation({
-            onError: () => {
-              // todo: any rollback?
-              toast.error("消息回复出错，请刷新后重试！")
-            },
-            onSuccess: (data) => {
-              data.forEach(({ requestId, result }) => {
-                if (!result) return toast.error(`App ${requestId} 出错`)
-                this.pApps.find((p) => p.id === requestId)!.needFetchLLM = true
-              })
-            },
-          })
-          .mutate({ conversationId, query })
-    }
-   }
-
-   public useQueryInHomePage () {
-  const addConversation = this.useAddConversation()
-  const queryInChatLayout = this.useQueryInChatLayout()
-  return (query: string) => {
-    if (!query) return toast.error("不能为空")
     // optimistic update
-    const conversationId = addConversation()
-    queryInChatLayout(query, conversation)
-  }
-},
-}
+    messages.push({
+      id: nanoid(),
+      updatedAt: new Date(),
+      content: query,
+      role: "user",
+      conversationId,
+      pAppId: null,
+      parentId: null,
+    })
 
+    api.llm.queryConversation
+      .useMutation({
+        onError: () => {
+          // todo: any rollback?
+          toast.error("消息回复出错，请刷新后重试！")
+        },
+        onSuccess: (data) => {
+          data.forEach(({ requestId, result }) => {
+            if (!result) return toast.error(`App ${requestId} 出错`)
+            pApps.find((p) => p.id === requestId)!.needFetchLLM = true
+          })
+        },
+      })
+      .mutate({ conversationId, query })
+  },
+
+  useQueryInHomePage() {
+    const addConversation = this.useAddConversation()
+    const queryInChatLayout = this.useQueryInChatLayout()
+    return (query) => {
+      if (!query) return toast.error("不能为空")
+      // optimistic update
+      const conversationId = addConversation()
+      const conversation = this.conversations.find(
+        (c) => c.id === conversationId,
+      )
+      if (!conversation) return toast.error("未找到会话")
+      queryInChatLayout(query, conversation)
+    }
+  },
+})
