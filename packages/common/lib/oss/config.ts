1e093c4c84cf30fe6f6e469a35e3cf44a4c21661
markshawn2020 <shawninjuly@gmail.com>
2024-03-07 06:54:39 +0800

    simpler ali ak/sk

--- packages/common/lib/oss/config.ts
+++ packages/common/lib/oss/config.ts
@@ -5,8 +5,8 @@ import { OSS_BUCKET_NAME, OSS_REGION } from "./const"
 // ref: https://help.aliyun.com/zh/oss/developer-reference/initialization-10#783f1f604f969
 export const oss = new OSS({
   // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-  accessKeyId: env.OSS_ACCESS_KEY_ID,
-  accessKeySecret: env.OSS_ACCESS_KEY_SECRET,
+  accessKeyId: env.ALI_AK,
+  accessKeySecret: env.ALI_SK,
   // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
   region: OSS_REGION,
   // yourBucketName填写Bucket名称。
