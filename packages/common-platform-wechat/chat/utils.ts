275c89db84517a72786bb4f624999685134b7093
markshawn2020 <shawninjuly@gmail.com>
2024-04-02 06:16:02 +0800

    better wechat

--- packages/common-platform-wechat/chat/utils.ts
+++ packages/common-platform-wechat/chat/utils.ts
@@ -1,3 +1,47 @@
+import { IMedia } from "@/schema/card"
+import { IUserSummary } from "@/schema/user.summary"
+import parse from "node-html-parser"
+import { api } from "../../common-api"
+import { parseMetaFromHtml } from "../../common-html/utils"
+import { html2md } from "../../common-markdown/html2md"
+
+export const parseWechatArticle = async (sourceUrl: string) => {
+  // 1. fetch page
+  const { data: pageText } = await api.get<string>(sourceUrl)
+
+  // 2. parse page
+  const html = parse(pageText)
+  const ogUrl = parseMetaFromHtml(html, "og:url", "property")
+
+  // 2.1. get id(sn) from page
+  const platformId = /sn=(.*?)&/.exec(ogUrl ?? "")![1]!
+
+  const contentHtml = html.getElementById("page-content")?.innerHTML ?? null
+  const title = parseMetaFromHtml(html, "og:title")
+  const coverUrl = parseMetaFromHtml(html, "og:image")!
+  const cover: IMedia = {
+    url: coverUrl,
+  }
+  // const source = "公众号"
+  const source = parseMetaFromHtml(html, "og:site_name") // 微信公众平台
+  const time =
+    new Date(Number(/var ct = "(.*?)"/.exec(pageText)?.[1]) * 1e3) ?? null // 1711455495
+  const author: IUserSummary = {
+    name: parseMetaFromHtml(html, "author", "name"),
+    image: /var hd_head_img = "(.*?)"/.exec(pageText)?.[1] ?? null,
+    id: /var user_name = "(.*?)"/.exec(pageText)?.[1] ?? "",
+  }
+
+  return {
+    platformId,
+    author,
+    time,
+    title,
+    cover,
+    contentMd: html2md(contentHtml ?? ""),
+  }
+}
+
 export const parseUrlFromWechatUrlMessage = (text: string): string | null => {
   const m = /<url>(.*?)<\/url>/.exec(text)
   return m?.[1] ?? null
--- src/core/card-platform/wechat-article/fetch-with-cache.action.ts
+++ src/core/card-platform/wechat-article/fetch-with-cache.action.ts
@@ -1,13 +1,10 @@
 "use server"
 
-import parse from "node-html-parser"
-import { api } from "../../../../packages/common-api"
+import { Prisma } from "@prisma/client"
 import { fetchArticleSummary } from "../../../../packages/common-article/core"
 import { IArticleSummaryParsed } from "../../../../packages/common-article/schema"
 import { parseSummary } from "../../../../packages/common-article/utils"
 import { prisma } from "../../../../packages/common-db/providers/prisma"
-import { parseMetaFromHtml } from "../../../../packages/common-html/utils"
-import { html2md } from "../../../../packages/common-markdown/html2md"
 import {
   fetchWechatArticleComments,
   fetchWechatArticleStat,
@@ -16,59 +13,30 @@ import {
   IWechatArticleComment,
   IWechatArticleStat,
 } from "../../../../packages/common-platform-wechat/article/detail/schema"
-import { ICardPlatform, IMedia } from "../../../schema/card"
+import { parseWechatArticle } from "../../../../packages/common-platform-wechat/chat/utils"
+import { ICardPlatform } from "../../../schema/card"
 import { cardBasicSchema } from "../../../schema/card.basic"
-import { IUserSummary } from "../../../schema/user.summary"
 import { ICardGenOptions } from "../../../store/card.atom"
 import { $Enums } from ".prisma/client"
-import PlatformType = $Enums.PlatformType
 
 export const fetchWechatArticleAction = async (
   sourceUrl: string,
   options: ICardGenOptions,
 ) => {
-  const platformType: PlatformType = "wechatArticle"
+  const platformType: $Enums.PlatformType = "wechatArticle"
   console.log("-- fetchWechatArticle: ", { url: sourceUrl })
 
-  // 1. fetch page
-  const { data: pageText } = await api.get<string>(sourceUrl)
-
-  // 2. parse page
-  const html = parse(pageText)
-  const ogUrl = parseMetaFromHtml(html, "og:url", "property")
-
-  // 2.1. get id(sn) from page
-  const platformId = /sn=(.*?)&/.exec(ogUrl ?? "")![1]!
+  const { platformId, contentMd, author, time, title, cover } =
+    await parseWechatArticle(sourceUrl)
 
   // 2.1.1 get data from id
   const dataInDB = await prisma.card.findUnique({
     where: { platformType_platformId: { platformType, platformId } },
   })
 
-  const contentHtml = html.getElementById("page-content")?.innerHTML ?? null
-  const title = parseMetaFromHtml(html, "og:title")
-  const coverUrl = parseMetaFromHtml(html, "og:image")!
-  const cover: IMedia = {
-    url: coverUrl,
-  }
-  // const source = "公众号"
-  const source = parseMetaFromHtml(html, "og:site_name") // 微信公众平台
-  const time =
-    new Date(Number(/var ct = "(.*?)"/.exec(pageText)?.[1]) * 1e3) ?? null // 1711455495
-  const author: IUserSummary = {
-    name: parseMetaFromHtml(html, "author", "name"),
-    image: /var hd_head_img = "(.*?)"/.exec(pageText)?.[1] ?? null,
-    id: /var user_name = "(.*?)"/.exec(pageText)?.[1] ?? "",
-  }
-  let comments: IWechatArticleComment[] | null | undefined = undefined
-  let stat: IWechatArticleStat | null | undefined = undefined
-  let contentMd: string | null | undefined = undefined
   let summary: IArticleSummaryParsed | null | undefined = undefined
-  const platformData: ICardPlatform<"wechatArticle"> = {}
 
-  if (contentHtml) {
-    contentMd = html2md(contentHtml)
-
+  if (contentMd) {
     // 2.1. cache summary
     if (!options.summary.cacheIgnored) {
       summary = dataInDB?.summary
@@ -83,6 +51,9 @@ export const fetchWechatArticleAction = async (
     }
   }
 
+  const platformData: ICardPlatform<"wechatArticle"> = {}
+  let comments: IWechatArticleComment[] | null | undefined = undefined
+  let stat: IWechatArticleStat | null | undefined = undefined
   // 3.1. stat
   if (options.stat.enabled) {
     if (!options.stat.cacheIgnored) stat = dataInDB?.stat
@@ -97,55 +68,28 @@ export const fetchWechatArticleAction = async (
       comments = (await fetchWechatArticleComments(platformId)).data
   }
 
-  const wechatArticleInDB = await prisma.card.upsert({
-    where: { platformType_platformId: { platformId, platformType } },
-    create: {
-      platformType,
-      platformId,
-
-      author,
-      time,
-      title,
-      cover,
-      stat,
-      platformData,
-      contentMd,
-      summary,
-      sourceUrl,
-    },
-    update: {
-      author,
-      time,
-      title,
-      cover,
-      stat,
-      platformData,
-      contentMd,
-      summary,
-      sourceUrl,
-    },
-    ...cardBasicSchema,
-  })
-
-  console.log("-- wechat article upserted: ", {
+  const data: Prisma.CardUncheckedCreateInput = {
+    platformType,
     platformId,
-    sourceUrl,
-    source,
     author,
     time,
     title,
     cover,
-    length: {
-      html: contentHtml?.length,
-      md: contentMd?.length,
-    },
+    platformData,
+    contentMd,
     summary,
     stat,
-    comments: {
-      length: comments?.length,
-      first: comments?.[0],
-    },
+    sourceUrl,
+  }
+  const wechatArticleInDB = await prisma.card.upsert({
+    where: { platformType_platformId: { platformId, platformType } },
+    create: data,
+    update: data,
+    ...cardBasicSchema,
   })
+
+  const { id } = wechatArticleInDB
+  console.log(`-- card upserted: `, { id, platformId, platformType })
 
   return wechatArticleInDB
 }
