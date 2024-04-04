ab1d4a6d9d987b45202628f87abc40fe6c79cc0a
markshawn2020 <shawninjuly@gmail.com>
2024-04-04 18:26:46 +0800

    fixed build

--- src/app/test/wechaty/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FlexContainer } from "packages/common-ui/components/flex-container"
-import { createBot } from "../../../../packages/common-platform-wechat/chat/core"
-
-export default async function TestWechatyPage() {
-  const bot = await createBot({ name: "mark" })
-  console.log("-- bot: ", bot)
-
-  return <FlexContainer>Test</FlexContainer>
-}
--- src/config/system.ts
+++ src/config/system.ts
@@ -1,5 +1,5 @@
 export const config = {
-  version: "V0.4.8 (α)",
+  version: "V0.4.9 (α)",
 
   // 取个什么产品名好🤔
   name: "",
