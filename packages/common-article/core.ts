d6f4fe2208b64a13bc45066fbfa6924d7ffa029f
markshawn2020 <shawninjuly@gmail.com>
2024-04-04 19:24:37 +0800

    -

--- packages/common-article/core.ts
+++ packages/common-article/core.ts
@@ -9,13 +9,14 @@ export type IAgentResponse = {
 }
 
 export const fetchArticleSummary = async (content: string) => {
-  const { data } = await api.postForm<IAgentResponse>(
-    `${env.NEXT_PUBLIC_BACKEND_URL}/agent/call`,
-    {
-      agent_type: "summariser",
-      model_type: "gpt-4",
-      content,
-    },
-  )
+  const url = `${env.NEXT_PUBLIC_BACKEND_URL}/agent/call`
+  const body = {
+    agent_type: "summariser",
+    model_type: "gpt-4",
+    content,
+  }
+  console.log("-- fetching summary: ", { url, body })
+
+  const { data } = await api.postForm<IAgentResponse>(url, body)
   return data
 }
