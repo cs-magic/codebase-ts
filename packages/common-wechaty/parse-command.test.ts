ff5c43d49fd9e77d1beb10403e26a72fe663cb9e
markshawn2020 <shawninjuly@gmail.com>
2024-04-11 17:35:29 +0800

    fixed parse-command

--- packages/common-wechaty/parse-command.test.ts
+++ packages/common-wechaty/parse-command.test.ts
@@ -1,17 +1,27 @@
 import { parseCommand } from "./parse-command"
 
-it("", () => {
-  const result = parseCommand("/set-backend-engine-type  nodejs", [
-    "set-backend-engine-type",
-  ])
-  expect(result.command).toBe("set-backend-engine-type")
-  expect(result.args).toBe("nodejs")
-})
+describe("parse command", () => {
+  it("", () => {
+    const result = parseCommand("/set-backend-engine-type", [
+      "set-backend-engine-type",
+    ])
+    expect(result.command).toBe("set-backend-engine-type")
+    expect(result.args).toBe("")
+  })
 
-it("", () => {
-  const result = parseCommand("/set-backend-engine-type  ", [
-    "set-backend-engine-type",
-  ])
-  expect(result.command).toBe("set-backend-engine-type")
-  expect(result.args).toBe("")
+  it("", () => {
+    const result = parseCommand("/set-backend-engine-type  ", [
+      "set-backend-engine-type",
+    ])
+    expect(result.command).toBe("set-backend-engine-type")
+    expect(result.args).toBe("")
+  })
+
+  it("", () => {
+    const result = parseCommand("/set-backend-engine-type  nodejs ss ", [
+      "set-backend-engine-type",
+    ])
+    expect(result.command).toBe("set-backend-engine-type")
+    expect(result.args).toBe("nodejs ss")
+  })
 })
--- packages/common-wechaty/parse-command.ts
+++ packages/common-wechaty/parse-command.ts
@@ -3,12 +3,17 @@ export const parseCommand = <T extends string>(
   commands: T[],
 ): { command?: T; args?: string } => {
   // 正则使用 `` 而非 // 的时候要 \s --> \\s
-  const m = new RegExp(`^/(${commands.join("|")})\\s*(.*?)\\s*$`).exec(text)
+  // - /A, ok
+  // - /A b, ok
+  // - /Ab, not ok
+  const m = new RegExp(`^/(${commands.join("|")})(?:\\s+(.*?))?\\s*$`).exec(
+    text,
+  )
 
   if (!m) return {}
 
   const command = m[1] as T
-  const args = m[2]
+  const args = m[2] ?? ""
   console.log({ text, command, args })
   return { command, args }
 }
