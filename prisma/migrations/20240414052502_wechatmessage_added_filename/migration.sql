695e555e2d2323d0c7e12a673d91456e00ec3b95
markshawn2020 <shawninjuly@gmail.com>
2024-04-14 13:25:27 +0800

    filename

---
+++ prisma/migrations/20240414052502_wechatmessage_added_filename/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "WechatMessage" ADD COLUMN     "filename" TEXT;
--- src/config/system.ts
+++ src/config/system.ts
@@ -1,5 +1,5 @@
 export const config = {
-  version: "V0.5.134 (α)",
+  version: "V0.5.135 (α)",
 
   // 取个什么产品名好🤔
   name: "",
