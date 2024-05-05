78d32467a89b8b8b9a60f98014f23215e65e832a
markshawn2020 <shawninjuly@gmail.com>
2024-05-05 13:40:08 +0800

    -:

--- packages-to-classify/llm/utils/calculate-token.ts
+++ packages-to-classify/llm/utils/calculate-token.ts
@@ -48,7 +48,12 @@ export const trimMessages = (messages: ILlmMessage[], model: LlmModelType) => {
   // todo: auto max len based on model
   const targetLen = 6e3 - 100
   const curLen = calculateToken(messages, model)
-  if (curLen > targetLen) {
+  if (
+    curLen > targetLen ||
+    // 第一条必须是system 或者 user
+    // todo: 放到 call 里
+    (!!messages.length && !["system", "user"].includes(messages[0]!.role))
+  ) {
     logger.debug(`trimming messages(curLen=${curLen}, targetLen=${targetLen})`)
     messages.splice(0, 1)
     trimMessages(messages, model)
