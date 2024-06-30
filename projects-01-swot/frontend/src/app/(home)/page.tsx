a8cb567f8a5de1c9335df07dd8804feb1a9008dd
markshawn2020 <shawninjuly@gmail.com>
2024-06-30 11:47:06 +0800

    -”

--- projects-01-swot/frontend/src/app/(home)/page.tsx
+++ projects-01-swot/frontend/src/app/(home)/page.tsx
@@ -1,17 +1,5 @@
-import { SwotLogoPng } from "@cs-magic/common/deps/ui/assets"
 import { FlexContainer } from "@cs-magic/common/deps/ui/components/flex-container"
-import Image from "next/image"
 
 export default async function HomePage() {
-  return (
-    <FlexContainer>
-      <Image
-        src={SwotLogoPng}
-        alt={""}
-        width={240}
-        height={240}
-        className={"h-auto"}
-      />
-    </FlexContainer>
-  )
+  return <FlexContainer></FlexContainer>
 }
--- projects-01-swot/frontend/src/components/branding-title.tsx
+++ projects-01-swot/frontend/src/components/branding-title.tsx
@@ -1,3 +1,5 @@
+import { SwotLogoPng } from "@cs-magic/common/deps/ui/assets"
+import Image from "next/image"
 import Link from "next/link"
 import { HTMLAttributes } from "react"
 import { cn } from "@cs-magic/common/deps/ui-shadcn/utils"
@@ -13,13 +15,20 @@ export const BrandingTitle = ({
     <Link
       href={"/"}
       className={cn(
-        "flex justify-center",
+        "flex justify-center items-center",
         // "w-full" ,
         // "w-[240px]",
         "shrink-0",
         // " bg-cyan-800",
       )}
     >
+      <Image
+        src={SwotLogoPng}
+        alt={""}
+        width={36}
+        height={36}
+        className={"h-auto"}
+      />
       <h1
         className={cn(
           "primary-gradient flex gap-4 text-lg font-bold sm:text-2xl",
