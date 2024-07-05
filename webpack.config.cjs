98c8c39ea3b7e59fcb36a7dd0d7432903c75ebbd
markshawn2020 <shawninjuly@gmail.com>
2024-07-05 15:27:43 +0800

    -

---
+++ webpack.config.cjs
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './apps/swot-bot/dist/src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
