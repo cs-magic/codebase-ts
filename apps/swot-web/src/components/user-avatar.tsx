9f249192547fd4a183ff41772e3d30457e15ed99
markshawn2020 <shawninjuly@gmail.com>
2024-07-25 08:03:17 +0800

    fixed avatar not display

--- apps/swot-web/src/components/user-avatar.tsx
+++ apps/swot-web/src/components/user-avatar.tsx
@@ -23,11 +23,14 @@ export const UserAvatar = ({
     imageProps?: ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
     avatarProps?: ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
   }) => {
+  const avatarUrl =
+    imageProps?.src ?? upgradeUrl(user?.image ?? user?.avatar ?? "")
+
   return (
     <div className={cn("flex h-full items-center gap-2", className)} {...props}>
       <VerticalAspectRatio ratio={1}>
         <Avatar
-          className={cn("h-full w-full border-none", avatarProps?.className)}
+          className={cn("h-full w-full border-none ", avatarProps?.className)}
           {...omit(avatarProps, ["className"])}
         >
           {/*<AvatarImage*/}
@@ -36,21 +39,29 @@ export const UserAvatar = ({
           {/*  }*/}
           {/*  {...omit(imageProps, ["src"])}*/}
           {/*/>*/}
-          <Image
-            alt={"user-avatar"}
-            width={120}
-            height={120}
-            className={"rounded-full "}
-            src={
-              imageProps?.src ?? upgradeUrl(user?.image ?? user?.avatar ?? "")
-            }
-            {...omit(imageProps, ["src", "width", "height"])}
-          />
+
+          {avatarUrl && (
+            <Image
+              alt={"user-avatar"}
+              width={120}
+              height={120}
+              className={"rounded-full "}
+              src={avatarUrl}
+              {...omit(imageProps, ["src", "width", "height"])}
+            />
+          )}
+
           <AvatarFallback>
             {user?.name?.length ? (
-              user.name.slice(0, 2)
+              <div
+                className={
+                  "h-full w-full bg-gray-50 border-none flex items-center justify-center text-xl"
+                }
+              >
+                {user.name.slice(0, 2).toUpperCase()}
+              </div>
             ) : (
-              <UserIcon className={"h-full w-full"} />
+              <UserIcon className={"h-full w-full bg-gray-50 border-none"} />
             )}
           </AvatarFallback>
         </Avatar>
