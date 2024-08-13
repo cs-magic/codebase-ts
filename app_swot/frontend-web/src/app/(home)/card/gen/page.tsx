dd0914900ef30147e2fed7bcd7e93a6e22adf5fb
markshawn2020 <shawninjuly@gmail.com>
2024-08-13 21:40:25 +0800

    fixed card gen

--- app_swot/frontend-web/src/app/(home)/card/gen/page.tsx
+++ app_swot/frontend-web/src/app/(home)/card/gen/page.tsx
@@ -1,18 +1,9 @@
-import dynamic from "next/dynamic"
-
-// ref: https://nextjs.org/docs/messages/react-hydration-error#solution-2-disabling-ssr-on-specific-components
-const CardNoSSR = dynamic(
-  () => import("@cs-magic/common-frontend/dist/components/card.js"),
-  {
-    ssr: false,
-  },
-)
+import Card from "@cs-magic/common-frontend/dist/components/card.js"
 
 export const metadata = {
   title: "卡片渲染 | 飞脑",
 }
 
 export default async function GenCardViaFrontendPage() {
-  return <CardNoSSR />
-  // return "hello"
+  return <Card />
 }
