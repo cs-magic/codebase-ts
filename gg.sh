157ae060f3c0cecb9f12fee1c1d13e2f3f5d9b6f
markshawn2020 <shawninjuly@gmail.com>
2024-08-25 20:02:16 +0800

    ""

--- gg.sh
+++ gg.sh
@@ -3,7 +3,15 @@ function update() {
   echo "== GIT UPDATE STARTED =="
 
   echo "== GIT UPDATE SUBMODULES STARTED =="
-  git submodule foreach --quiet 'echo $path' | xargs -P 8 -I {} bash -c "git add . && git commit -m \"$1\"; git push"
+  git submodule foreach '
+    echo "Processing $name ($path)..."
+    if [ -z "$(git status --porcelain)" ]; then
+#      echo "The submodule $name is clean."
+    else
+      git add . && git commit -m \"$1\"; git push
+    fi
+  '
+#  git submodule foreach --quiet 'echo $path' | xargs -P 8 -I {} bash -c "git add . && git commit -m \"$1\"; git push"
 #  git submodule foreach --quiet 'echo $path' | parallel -j 8 "git add . && git commit -m \"$1\"; git push"
   echo "== GIT UPDATE SUBMODULES FINISHED =="
 
