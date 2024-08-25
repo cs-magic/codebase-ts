2d52a9b8289ebeb6a034f1f943c86c77e84fa91e
markshawn2020 <shawninjuly@gmail.com>
2024-08-25 19:58:26 +0800

    ""

---
+++ gg.sh
@@ -0,0 +1,16 @@
+function update() {
+  msg=$1
+  echo "== GIT UPDATE STARTED =="
+
+  echo "== GIT UPDATE SUBMODULES STARTED =="
+  git submodule foreach --quiet 'echo $path' | parallel -j 8 "git add . && git commit -m \"$1\"; git push"
+  echo "== GIT UPDATE SUBMODULES FINISHED =="
+
+  echo "== GIT UPDATE MAIN STARTED =="
+  git add . && git commit -m \"$1\"; git push
+  echo "== GIT UPDATE MAIN FINISHED =="
+
+  echo "== GIT UPDATE FINISHED =="
+}
+
+update
