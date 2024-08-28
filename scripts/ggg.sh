a3fba57dd14063c859b453f8c5398e8452c94f14
markshawn2020 <shawninjuly@gmail.com>
2024-08-28 23:34:21 +0800

    update

--- scripts/ggg.sh
+++ scripts/ggg.sh
@@ -10,11 +10,12 @@ function update() {
   echo ">> GIT UPDATE STARTED"
 
   echo ">> GIT UPDATE SUBMODULES STARTED"
-  git submodule foreach '
+  git submodule foreach bash -c '
+    echo "the message is $1"
     if [ -n "$(git status --porcelain)" ]; then
-      git add . && git commit -m "$msg"; git push
+      git add . && git commit -m "$1"; git push
     fi
-  '
+  ' _ "$msg"
 #  git submodule foreach --quiet 'echo $path' | xargs -P 8 -I {} bash -c "git add . && git commit -m \"$1\"; git push"
 #  git submodule foreach --quiet 'echo $path' | parallel -j 8 "git add . && git commit -m \"$1\"; git push"
   echo "<< GIT UPDATE SUBMODULES FINISHED"
