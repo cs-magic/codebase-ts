4e5dc98d8e156aa970daa7751640e06f1080bfb6
markshawn2020 <shawninjuly@gmail.com>
2024-08-28 22:23:55 +0800

    drop /bin/zsh

--- __scripts__/ggg.sh
+++ __scripts__/ggg.sh
@@ -12,9 +12,9 @@ function update() {
   echo ">> GIT UPDATE SUBMODULES STARTED"
   git submodule foreach '
     if [ -n "$(git status --porcelain)" ]; then
-      git add . && git commit -m "$0"; git push
+      git add . && git commit -m "$msg"; git push
     fi
-  ' "$msg"
+  '
 #  git submodule foreach --quiet 'echo $path' | xargs -P 8 -I {} bash -c "git add . && git commit -m \"$1\"; git push"
 #  git submodule foreach --quiet 'echo $path' | parallel -j 8 "git add . && git commit -m \"$1\"; git push"
   echo "<< GIT UPDATE SUBMODULES FINISHED"
