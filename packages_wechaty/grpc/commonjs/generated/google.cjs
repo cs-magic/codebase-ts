d9646a5772a80d0559d32cdf87bc2ef77d4d0c88
markshawn2020 <shawninjuly@gmail.com>
2024-08-31 04:01:25 +0800

    fixed wechaty grpc/puppet-service

--- packages_wechaty/grpc/commonjs/generated/google.cjs
+++ packages_wechaty/grpc/commonjs/generated/google.cjs
@@ -1,9 +1,14 @@
-const googleFileList = [
-  '../../out/google/api/health_check/v1/health_check_grpc_pb.js',
-  '../../out/google/api/health_check/v1/health_check_pb.js',
-]
+//const googleFileList = [
+//  '../../out/google/api/health_check/v1/health_check_grpc_pb.js',
+//  '../../out/google/api/health_check/v1/health_check_pb.js',
+//]
 
-module.exports = googleFileList.reduce((acc, pkg) => ({
-  ...acc,
-  ...require(pkg),
-}), {}) // Huan(202108): must provide a `{}` as the initial value, or it will be `[]`
+//module.exports = googleFileList.reduce((acc, pkg) => ({
+//  ...acc,
+//  ...require(pkg),
+//}), {}) // Huan(202108): must provide a `{}` as the initial value, or it will be `[]`
+
+module.exports = {
+  ...require("../../out/google/api/health_check/v1/health_check_grpc_pb.js"),
+  ...require("../../out/google/api/health_check/v1/health_check_pb.js"),
+};
