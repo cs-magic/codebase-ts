37447159d89336b9f106b395795405e704cbebb6
markshawn2020 <shawninjuly@gmail.com>
2024-09-03 18:51:45 +0800

    -

--- packages/common/src/utils/format-error.ts
+++ packages/common/src/utils/format-error.ts
@@ -1,9 +1,14 @@
-import { AxiosError } from "axios"
+import { AxiosError } from "axios";
 
-import logger from "../log/index.js"
+import logger from "../log/index.js";
 
 export const formatError = (e: unknown) => {
-  const s = e instanceof AxiosError ? JSON.stringify(e.response?.data) : e instanceof Error ? e.message : (e as string)
-  logger.error(`❌ 出错： ${s}`)
-  return s
-}
+  const s =
+    e instanceof AxiosError
+      ? JSON.stringify(e.response?.data)
+      : e instanceof Error
+        ? e.message
+        : (e as string);
+  logger.error(`❌ 出错： [${s}]`);
+  return s;
+};
