4c3380edb37fedc5c3bc6a4f3b8dc37799e8f48b
markshawn2020 <shawninjuly@gmail.com>
2024-09-05 23:12:30 +0800

    fixed neurora ui

--- packages_frontend/react/src/providers/screen.provider.tsx
+++ packages_frontend/react/src/providers/screen.provider.tsx
@@ -1,24 +1,26 @@
-"use client"
+"use client";
 
-import { useSetAtom } from "jotai"
-import React, { PropsWithChildren, useEffect } from "react"
-import { useMeasure } from "react-use"
+import { useSetAtom } from "jotai";
+import React, { PropsWithChildren, useEffect } from "react";
+import { useMeasure } from "react-use";
 
-import { uiScreenAtom } from "@/store/ui.atom"
+import { uiScreenAtom } from "@/store/ui.atom";
 
 export const ScreenProvider = ({ children }: PropsWithChildren) => {
-  const [ref, { width, height }] = useMeasure<HTMLDivElement>()
+  const [ref, { width, height }] = useMeasure<HTMLDivElement>();
 
-  const setMainArea = useSetAtom(uiScreenAtom)
+  const setMainArea = useSetAtom(uiScreenAtom);
 
   useEffect(() => {
-    setMainArea({ width, height })
+    setMainArea({ width, height });
     // console.log({ width, height })
-  }, [width, height])
+  }, [width, height]);
+
+  // return children;
 
   return (
     <div id={"screen-measure"} ref={ref}>
       {children}
     </div>
-  )
-}
+  );
+};
