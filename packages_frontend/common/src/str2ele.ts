941236f4ef7a3c7215bd6f8189f99519fd234b4d
markshawn2020 <shawninjuly@gmail.com>
2024-09-18 10:54:05 +0800

    [exts_chrome_claude-enhancer] archive

--- packages_frontend/common/src/str2ele.ts
+++ packages_frontend/common/src/str2ele.ts
@@ -1,5 +1,9 @@
-export function str2ele(str: string): ChildNode {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(str, "text/html");
-  return doc.body.firstChild!;
+export function str2ele(str: string): HTMLElement {
+  // const parser = new DOMParser();
+  // const doc = parser.parseFromString(str, "text/html");
+  // return doc.body.firstChild!;
+
+  const element = document.createElement("div");
+  element.innerHTML = str;
+  return element.firstElementChild as HTMLElement;
 }
