3b25ddac04be7ab4381f13774e562332cd8bf1da
markshawn2020 <shawninjuly@gmail.com>
2024-09-23 19:51:03 +0800

    better post-install script

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,7 +1,58 @@
-yarn db:generate
+#!/bin/bash
 
-yarn install:tspc
+init-db() {
+  echo ">> init db..."
 
-yarn build:wechaty
+  prisma generate
 
-yarn build:frontend
+  if [[ "$1" == "--local" ]]; then
+    prisma migrate dev
+  elif [[ "$1" == "--remote" ]]; then
+    prisma migrate deploy
+  else
+    echo "Error: You should specify which migration to run on prisma (--local or --remote)"
+    return 1
+  fi
+
+  echo "<< init db.\n"
+}
+
+init-commands() {
+  echo ">> init commands..."
+#  install tspc for build
+  ts-patch install -s
+  echo "<< init commands.\n"
+}
+
+init-wechaty()  {
+  echo ">> init wechaty..."
+
+#  wechaty-puppet
+  yarn workspace wechaty-puppet dist
+
+#  wechaty-puppet-wechat4u
+  yarn workspace wechat4u compile && yarn workspace wechaty-puppet-wechat4u dist
+
+#  wechaty-service
+  yarn workspace wechaty-grpc install:protoc && PATH=$PATH:$(go env GOPATH)/bin yarn workspace wechaty-grpc dist && yarn workspace wechaty-puppet-service dist
+
+#  wechaty
+  yarn workspace wechaty dist
+
+  echo "<< init wechaty.\n"
+}
+
+init-fullstack-packages() {
+  echo ">> init fullstack packages..."
+
+# build frontend, as well as backend
+  yarn workspace @cs-magic/react build
+
+  echo "<< init fullstack packages.\n"
+}
+
+
+init-db "$@"
+init-commands
+init-wechaty
+init-fullstack-packages
