9a7c3f2cbe6bba48f31f596b16bd47614986b8f8
markshawn2020 <shawninjuly@gmail.com>
2024-09-26 19:26:09 +0800

    chore: tspc --> tsc

--- packages_frontend/react/src/components/company-logo.tsx
+++ packages_frontend/react/src/components/company-logo.tsx
@@ -1,9 +1,18 @@
-import clsx from "clsx"
-import { HTMLProps } from "react"
+import clsx from "clsx";
+import { HTMLProps } from "react";
 
-import CsMagicBlackLogoSvg from "@/branding/cs-magic/cs-magic_logo_1280.svg"
-import { LOGO_SIZE_LG } from "@/config"
+// @transform-path
+import CsMagicBlackLogoSvg from "@assets/branding/cs-magic/cs-magic_logo_1280.svg";
+import { LOGO_SIZE_LG } from "@/config";
 
-export const CompanyLogo = ({ className, ...props }: HTMLProps<SVGSVGElement>) => (
-  <CsMagicBlackLogoSvg className={clsx("", className)} width={LOGO_SIZE_LG} height={LOGO_SIZE_LG} {...props} />
-)
+export const CompanyLogo = ({
+  className,
+  ...props
+}: HTMLProps<SVGSVGElement>) => (
+  <CsMagicBlackLogoSvg
+    className={clsx("", className)}
+    width={LOGO_SIZE_LG}
+    height={LOGO_SIZE_LG}
+    {...props}
+  />
+);
