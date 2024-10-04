7a4f13a22c6651f197d6043a9f1f9b8f776ebf2a
markshawn2020 <shawninjuly@gmail.com>
2024-10-04 20:38:16 +0800

    added submodule of assistant-pc

--- apps/assistant-pc/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// postcss.config.ts
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {}, // ref: https://tailwindcss.com/docs/using-with-preprocessors#nesting
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
