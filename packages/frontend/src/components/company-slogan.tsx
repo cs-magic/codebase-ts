95a70d0a8a5642624cdb75ca8b40538dc244c78b
markshawn2020 <shawninjuly@gmail.com>
2024-11-12 03:24:37 +0800

    uni-pusher better login

--- packages/frontend/src/components/company-slogan.tsx
+++ packages/frontend/src/components/company-slogan.tsx
@@ -1,6 +1,5 @@
 import { CompanyLogo } from "@/components/company-logo";
 
-import bitFont from "@assets/fonts/VonwaonBitmap-16px.ttf";
 import { cn } from "@cs-magic/shadcn/lib/utils";
 
 export const CompanySlogan = () => {
@@ -8,7 +7,7 @@ export const CompanySlogan = () => {
     <div className={"flex items-center gap-2 sm:gap-4 text-2xl"}>
       {/*<Image src={assets.Logo} alt={'logo'} width={36} height={36}/>*/}
       <CompanyLogo width={36} height={36} />
-      <p className={cn("text-base sm:text-xl font-light", bitFont.className)}>
+      <p className={cn("text-base sm:text-xl font-light")}>
         大模型时代，人人都是魔法师
       </p>
     </div>
