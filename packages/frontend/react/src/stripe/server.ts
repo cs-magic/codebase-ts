b0eb3ccb0023874b2185a97ce0b0ab32ac219b3e
markshawn2020 <shawninjuly@gmail.com>
2024-11-24 21:24:31 +0800

    fixed stripe

--- packages/frontend/react/src/stripe/server.ts
+++ packages/frontend/react/src/stripe/server.ts
@@ -1,14 +1,14 @@
-import "server-only";
-import Stripe from "stripe";
+import "server-only"
+import Stripe from "stripe"
 
-if (!process.env.STRIPE_API_KEY) throw new Error("no stripe api key");
+if (!process.env.STRIPE_API_KEY) throw new Error("no stripe api key")
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: "2023-08-16",
+  apiVersion: "2024-11-20.acacia",
   appInfo: {
     name: "poketto.ai",
     url: "https://poketto.ai",
     version: "0.1.0",
   },
-});
+})
